4b7259f16a46d82c1803bd8dbbd84a83ed2d8c6f
orf <tom@tomforb.es>
2014-03-31 23:16:23 +0100

    Added a ?tryme parameter that triggers random errors in the stream

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, url
 from django.views.generic import DetailView
+from django.http import HttpResponse
 
 from .views import KeyFeed, CookieFeed, Homepage
 from .models import TestFeedItem
@@ -7,6 +8,9 @@ from .models import TestFeedItem
 
 urlpatterns = patterns('',
                        url('^$', Homepage.as_view(template_name="index.html" )),
+                       url(r'^robots\.txt$',
+                           lambda r: HttpResponse("User-agent: *\nDisallow: /feed/*\nDisallow: /item/*",
+                                                  mimetype="text/plain")),
                        url('^item/(?P<id>\d+)', DetailView.as_view(pk_url_kwarg="id", model=TestFeedItem), name="view"),
                        url('^feed/(?P<feed_type>(rss|atom))/cookie', CookieFeed(), name="cookie"),
                        url('^feed/(?P<feed_type>(rss|atom))/(?P<key>\S+)$', KeyFeed(), name="key"),
--- feeds/views.py
+++ feeds/views.py
@@ -1,3 +1,5 @@
+from __future__ import division
+
 import uuid
 import datetime
 import random
@@ -6,8 +8,14 @@ from django.contrib.syndication.views import Feed
 from django.views.generic import TemplateView
 from django.utils.feedgenerator import Atom1Feed, Rss201rev2Feed
 from django.core.urlresolvers import reverse
-
+from django.http import HttpResponse, HttpResponseRedirect
+import string
 from .models import TestFeed, TestFeedItem
+
+
+class ScrewedUpResponse(HttpResponse):
+    def items(self):
+        return [("lol\n\n!", "o\n\br\0")]  # Send an invalid header to the client. Messes stuff up
 
 
 class Homepage(TemplateView):
@@ -24,10 +32,41 @@ class BaseFeed(Feed):
     def __call__(self, request, *args, **kwargs):
         self.feed_type_name = kwargs["feed_type"]
         self.feed_type = Rss201rev2Feed if self.feed_type_name == "rss" else Atom1Feed
-        return super(BaseFeed, self).__call__(request, *args, **kwargs)
+        self.simulate_issue = None
+
+        if random.randint(0, 100) > 75 and "tryme" in request.GET:
+            self.simulate_issue = random.choice(["500", "redirect", "non_xml",
+                                                 "invalid_xml", "non_http",
+                                                 "screwed_results", "future_dates"])
+
+            if self.simulate_issue == "500":
+                return HttpResponse("500 internal server error", status=500)
+            elif self.simulate_issue == "redirect":
+                return HttpResponseRedirect("http://google.com")
+            elif self.simulate_issue == "non_xml":
+                return HttpResponse("This is some non-xml content for you", content_type="text/plain")
+
+        result = super(BaseFeed, self).__call__(request, *args, **kwargs)
+
+        if self.simulate_issue == "invalid_xml":
+            # Replace a bunch of characters in the response text
+            resp_length = len(result.content)
+            content = list(result.content)
+            for i in xrange(int(resp_length / 10)):
+                idx_to_change = random.randint(0, resp_length)
+                content[idx_to_change] = random.choice(string.printable)
+
+            result.content = "".join(content)
+        elif self.simulate_issue == "non_http":
+            return ScrewedUpResponse()
+
+        return result
 
     def get_key(self, request, *args, **kwargs):
         raise NotImplementedError()
+
+    def get_total_seconds(self, elapsed):
+        return (elapsed.microseconds + (elapsed.seconds + elapsed.days*24*3600) * 1e6) / 1e6  # Python 2.6 compatability
 
     def get_object(self, request, *args, **kwargs):
         key = self.get_key(request, *args, **kwargs)
@@ -44,13 +83,17 @@ class BaseFeed(Feed):
         created_ago = (datetime.datetime.now() - last_item.created)
         if created_ago > datetime.timedelta(seconds=create_trigger):
             # Create a new item
-            random_time = created_ago + datetime.timedelta(seconds=random.randint(1, int(created_ago.total_seconds())))
+            random_time = created_ago + datetime.timedelta(seconds=random.randint(1, int(self.get_total_seconds(created_ago))))
             TestFeedItem.objects.create(feed=obj, display_time=random_time)
 
         return obj
 
     def items(self, obj):
-        return obj.items.order_by("-created")[:10]
+        items = obj.items
+        if self.simulate_issue == "screwed_results":
+            return items.order_by("?")[:10]
+
+        return items.order_by("-created")[:10]
 
     def item_title(self, item):
         return item.title
@@ -59,6 +102,9 @@ class BaseFeed(Feed):
         return item.content
 
     def item_pubdate(self, item):
+        if self.simulate_issue == "future_dates":
+            if random.randint(1, 2) == 2:
+                return datetime.datetime.now() + datetime.timedelta(minutes=random.randint(0, 60))
         return item.display_time
 
     def link(self, obj):
